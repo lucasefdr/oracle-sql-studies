56dad1779a55c13899fe3d0d01f96bfbec2d8bc9
Lucas <lcseferreira@hotmail.com>
2022-10-28 16:50:34 -0300

    Seção 7 - Sintaxe e diretrizes

--- Seção 6 - Variáveis e Constantes/3 - Tipos de Dados no PLSQL - Datatypes.sql
+++ Seção 6 - Variáveis e Constantes/3 - Tipos de Dados no PLSQL - Datatypes.sql
@@ -1,31 +1,30 @@
 --
--- Se√ß√£o 6 - Declara√ß√£o de Identificadores - Vari√°veis e Constantes
+-- SeÁ„o 6 - DeclaraÁ„o de Identificadores - Vari·veis e Constantes
 --
 -- Aula 3 - Tipos de Dados no PL/SQL - Datatypes
 --
 
--- Declarando Vari√°veis utilizando os principais Tipos de Dados
+-- Declarando Vari·veis utilizando os principais Tipos de Dados
 
-SET SERVEROUTPUT ON
 DECLARE
   vNumero              NUMBER(11,2) := 1200.50;
-  vCaracterTamFixo     CHAR(100) := 'Texto de Tamanho Fixo de at√© 32767 bytes';
-  vCaracterTamVariavel VARCHAR2(100) := 'Texto Tamanho vari√°vel de at√© 32767 bytes';
+  vCaracterTamFixo     CHAR(100) := 'Texto de Tamanho Fixo de atÈ 32767 bytes';
+  vCaracterTamVariavel VARCHAR2(100) := 'Texto Tamanho vari·vel de atÈ 32767 bytes';
   vBooleano            BOOLEAN := TRUE;
   vData                DATE := sysdate;
-  vLong                LONG := 'Texto Tamanho vari√°vel de at√© 32760 bytes';
+  vLong                LONG := 'Texto Tamanho vari·vel de atÈ 32760 bytes';
   vRowid               ROWID;
   vTimestamp           TIMESTAMP := systimestamp;
   vTimestamptz         TIMESTAMP WITH TIME ZONE := systimestamp;
-  vCaracterTamFixoUniversal     NCHAR(100) := 'Texto de Tamanho Fixo Universal de at√© 32767 bytes';
-  vCaracterTamVariavelUniversal NVARCHAR2(100) := 'Texto Tamanho vari√°vel Universal de at√© 32767 bytes';
+  vCaracterTamFixoUniversal     NCHAR(100) := 'Texto de Tamanho Fixo Universal de atÈ 32767 bytes';
+  vCaracterTamVariavelUniversal NVARCHAR2(100) := 'Texto Tamanho vari·vel Universal de atÈ 32767 bytes';
   vNumeroInteiro       BINARY_INTEGER := 1200;
   vNumeroFloat         BINARY_FLOAT := 15000000;
   vNumeroDouble        BINARY_DOUBLE := 15000000;
 BEGIN
   DBMS_OUTPUT.PUT_LINE('Numero = ' ||   vNumero);
   DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Fixo = ' || vCaracterTamFixo );
-  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Vari√°vel = ' || vCaracterTamVariavel );
+  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Vari·vel = ' || vCaracterTamVariavel );
   IF  vBooleano = TRUE
   THEN 
     DBMS_OUTPUT.PUT_LINE('Booleano = ' || 'TRUE');
@@ -42,7 +41,7 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('Timestamp = ' || vTimestamp);
   DBMS_OUTPUT.PUT_LINE('Timestamp with time zone= ' || vTimestamptz);
   DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Fixo = ' || vCaracterTamFixoUniversal );
-  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Vari√°vel = ' || vCaracterTamVariavelUniversal );
+  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Vari·vel = ' || vCaracterTamVariavelUniversal );
   DBMS_OUTPUT.PUT_LINE('Numero Inteiro = ' || vNumeroInteiro);
   DBMS_OUTPUT.PUT_LINE('Numero Float = ' || vNumeroFloat);
   DBMS_OUTPUT.PUT_LINE('Numero Double = ' || vNumeroDouble);
